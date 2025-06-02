fba9ebc2d43004379f2cf62b52a8a384d4a2e50c
mana2518 <mana2518@gmail.com>
2025-06-03 00:27:29 +0900

    Improve error message display and content extraction

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ async function summarizePage() {
         
         // URLからHTMLを取得
         const response = await fetch(urlInput.value, {
+            method: 'GET',
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
