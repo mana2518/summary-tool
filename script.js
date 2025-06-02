9c5f889c23093a9b2808d0e8b2bb95cf6c85a86c
mana2518 <mana2518@gmail.com>
2025-06-03 00:37:45 +0900

    Add CORS proxy service

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ async function summarizePage() {
             throw new Error('有効なURLを入力してください（http://またはhttps://から始まるURL）');
         }
 
-        // URLからHTMLを取得
-        const response = await fetch(urlInput.value, {
+        // プロキシサービスを使用してURLからHTMLを取得
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/' + urlInput.value;
+        const response = await fetch(proxyUrl, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
